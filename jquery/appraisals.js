8313df887b92531236c955fe104892db740778c3
Maria Narvaez <narvaezmaria25@gmail.com>
2016-08-11 00:51:10 -0400

    final project

---
+++ jquery/appraisals.js
@@ -0,0 +1,88 @@
+$(document).ready(function () {
+
+
+
+	
+
+	
+$(".squares1").on("mouseenter", function(){
+    $(".box1").show();
+}).on("mouseleave", function(){
+    $(".box1").hide();
+});
+
+
+	
+$(".squares2").on("mouseenter", function(){
+    $(".box2").show();
+}).on("mouseleave", function(){
+    $(".box2").hide();
+});
+
+
+$(".squares3").on("mouseenter", function(){
+    $(".box3").show();
+}).on("mouseleave", function(){
+    $(".box3").hide();
+});
+
+
+	
+$(".squares4").on("mouseenter", function(){
+    $(".box4").show();
+}).on("mouseleave", function(){
+    $(".box4").hide();
+});
+
+
+$(".squares5").on("mouseenter", function(){
+    $(".box5").show();
+}).on("mouseleave", function(){
+    $(".box5").hide();
+});
+
+
+	
+$(".squares6").on("mouseenter", function(){
+    $(".box6").show();
+}).on("mouseleave", function(){
+    $(".box6").hide();
+});
+
+
+$(".squares7").on("mouseenter", function(){
+    $(".box7").show();
+}).on("mouseleave", function(){
+    $(".box7").hide();
+});
+
+$(".squares8").on("mouseenter", function(){
+    $(".box8").show();
+}).on("mouseleave", function(){
+    $(".box8").hide();
+});
+
+	
+$(".squares9").on("mouseenter", function(){
+    $(".box9").show();
+}).on("mouseleave", function(){
+    $(".box9").hide();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
---
+++ jquery/index.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+	
+
+
+  // if (screen.width() > 500) {
+
+  //   $(".desktop").hide();
+
+  // } else {
+
+  //   $(".desktop").show();
+
+  //   }
+
+ 
+
+
+	// $("h1").hide();
+
+	// $("h1").fadeIn(2000);
+
+});
+
